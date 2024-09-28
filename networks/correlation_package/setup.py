17853d08e70f4036c1d104368bee31deeab5f80e
Quang Vu <vhquang@gmail.com>
2024-09-28 12:49:24 -0700

    Try to complie and install layers

--- networks/correlation_package/setup.py
+++ networks/correlation_package/setup.py
@@ -5,7 +5,8 @@ import torch
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-cxx_args = ['-std=c++11']
+# cxx_args = ['-std=c++11']
+cxx_args = ['-std=c++17']
 
 nvcc_args = [
     '-gencode', 'arch=compute_50,code=sm_50',
@@ -22,7 +23,8 @@ setup(
         CUDAExtension('correlation_cuda', [
             'correlation_cuda.cc',
             'correlation_cuda_kernel.cu'
-        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args})
+        ], 
+        extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args})
     ],
     cmdclass={
         'build_ext': BuildExtension
